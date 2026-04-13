04145e14bcae2ee0161e10cc2e94fabcb37e0d10
matuhac <matuh.uhlir@gmail.com>
2026-04-13 07:55:32 +0200

    Pridani souboru - Kicad a CubeIDE
    Pridani uz vytvorenych souboru.

---
+++ My_Project/main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author matuh
+ * @date 2026-04-12
+ * @brief Main function
+ */
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+
+    return 0;
+}
